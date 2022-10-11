2f2965c4db7bea2d9f439de3f313206056652aef
A Mahmud Umer <94960783+umerthedev@users.noreply.github.com>
2022-10-11 19:58:49 +0600

    database updated

--- database/laravel_phonebook.sql
+++ database/laravel_phonebook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2022 at 07:25 PM
+-- Generation Time: Oct 11, 2022 at 03:58 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -51,7 +51,10 @@ INSERT INTO `contactlists` (`id`, `name`, `email`, `address`, `phone`, `image`, 
 (25, 'Omar Faruk', 'cute@hackermail.com', '123 kazipara, Mirpur Dhaka', '01923409799', '1665316140.PNG', 'Amahamudumer', 'Amahamudumer', '1', '0', '2022-10-09 05:49:00', '2022-10-09 05:49:00'),
 (26, 'amanullah', 'amanullah@gmail.com', '123 kazipara, Mirpur Dhaka', '01765678976', '1665337314.jpg', 'amanullah', 'amanullah', '1', '0', '2022-10-09 11:41:54', '2022-10-09 11:41:54'),
 (27, 'Manegment apple', 'admin@gmail.com', 'dhanmondi, dhaka', '01765678976', '1665418214.jpg', 'asdsad', 'Amahamudumer', '1', '0', '2022-10-10 10:10:14', '2022-10-10 10:10:14'),
-(28, 'Dr B. Sick', 'designguruam@gmail.com', '123 kazipara, Mirpur Dhaka', '4343434534', '1665418322.PNG', 'amanullah', 'amanullah', '1', '0', '2022-10-10 10:12:02', '2022-10-10 10:12:02');
+(28, 'Dr B. Sick', 'designguruam@gmail.com', '123 kazipara, Mirpur Dhaka', '4343434534', '1665418322.PNG', 'amanullah', 'amanullah', '1', '0', '2022-10-10 10:12:02', '2022-10-10 10:12:02'),
+(29, 'Ribol', 'ribol@gmail.com, umer@gmail.com', '123 kazipara, Mirpur Dhaka', '01765678976, 013224244', '1665495699.jpg', 'amanullah', 'amanullah', '1', '0', '2022-10-11 07:41:39', '2022-10-11 07:45:32'),
+(30, 'sohan', 'sohan@gmail.com', 'komolapur, dhaka', '01765678976', '1665496557.jpg', 'amanullah', 'asdasd', '1', '0', '2022-10-11 07:55:57', '2022-10-11 07:55:57'),
+(31, 'akash', 'akash@gmail.com', 'kazipara, mirpur,dhaka', '0156789544', '1665496601.jpg', 'Amahamudumer', 'asdasssa', '1', '0', '2022-10-11 07:56:41', '2022-10-11 07:56:41');
 
 -- --------------------------------------------------------
 
@@ -97,7 +100,10 @@ CREATE TABLE `favoritecontacts` (
 INSERT INTO `favoritecontacts` (`id`, `name`, `email`, `address`, `phone`, `image`, `fb_id`, `ig_id`, `user_id`, `contact_id`, `created_at`, `updated_at`) VALUES
 (4, 'Omar Faruk', 'cute@hackermail.com', '123 kazipara, Mirpur Dhaka', '01923409799', '1665316140.PNG', 'Amahamudumer', 'Amahamudumer', '1', '25', '2022-10-10 11:25:16', '2022-10-10 11:25:16'),
 (5, 'amanullah', 'amanullah@gmail.com', '123 kazipara, Mirpur Dhaka', '01765678976', '1665337314.jpg', 'amanullah', 'amanullah', '1', '26', '2022-10-10 11:25:18', '2022-10-10 11:25:18'),
-(6, 'Manegment apple', 'admin@gmail.com', 'dhanmondi, dhaka', '01765678976', '1665418214.jpg', 'asdsad', 'Amahamudumer', '1', '27', '2022-10-10 11:25:20', '2022-10-10 11:25:20');
+(7, 'Manegment apple', 'admin@gmail.com', 'dhanmondi, dhaka', '01765678976', '1665418214.jpg', 'asdsad', 'Amahamudumer', '1', '27', '2022-10-11 07:52:53', '2022-10-11 07:52:53'),
+(10, 'Dr B. Sick', 'designguruam@gmail.com', '123 kazipara, Mirpur Dhaka', '4343434534', '1665418322.PNG', 'amanullah', 'amanullah', '1', '28', '2022-10-11 07:54:39', '2022-10-11 07:54:39'),
+(11, 'akash', 'akash@gmail.com', 'kazipara, mirpur,dhaka', '0156789544', '1665496601.jpg', 'Amahamudumer', 'asdasssa', '1', '31', '2022-10-11 07:57:01', '2022-10-11 07:57:01'),
+(12, 'sohan', 'sohan@gmail.com', 'komolapur, dhaka', '01765678976', '1665496557.jpg', 'amanullah', 'asdasd', '1', '30', '2022-10-11 07:57:06', '2022-10-11 07:57:06');
 
 -- --------------------------------------------------------
 
@@ -176,7 +182,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('tkrC1Q4DUg3W6inxVkJl0bXqxHUrZY6vc3Gi29KF', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoibDNGdEJwbFk5b2ZHemFJeGtJTTBjUHdlVTdZS3IyaG1VRFl4SEs5ZiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Njc6Imh0dHA6Ly9sb2NhbGhvc3QvbGFyYXZlbC9sYXJhdmVsX3Bob25lYm9vay9wdWJsaWMvZmF2b3JpdGVfY29udGFjdHMiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1665422740);
+('pB9blsJFfnTlqJ50ri1FFQ2vazdJTXedds8X1vxJ', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiNmVLYkQ4b0dRcUtPRGhvN2xVV2NJejNtZWg1QkNDcU9sRzBjSmoxYiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NjI6Imh0dHA6Ly9sb2NhbGhvc3QvbGFyYXZlbC9sYXJhdmVsX3Bob25lYm9vay9wdWJsaWMvc2hvd19jb250YWN0Ijt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1665488278),
+('WrXccrSr4lHfJNVhg24HhFAUGquR2ou6irJT6TYa', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiZ2gxTXVwc3F3NFNtNmZ2R3dta0h6MzhvS05iaUU3dHpVTlVWMjFzbSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NTQ6Imh0dHA6Ly9sb2NhbGhvc3QvbGFyYXZlbC9sYXJhdmVsX3Bob25lYm9vay9wdWJsaWMvSG9tZSI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1665496685);
 
 -- --------------------------------------------------------
 
@@ -275,7 +282,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `contactlists`
 --
 ALTER TABLE `contactlists`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -287,7 +294,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `favoritecontacts`
 --
 ALTER TABLE `favoritecontacts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `migrations`
